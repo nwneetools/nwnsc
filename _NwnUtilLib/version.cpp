b8850e33b100fdd0d3d398e8f9fa5d4c0e42e3ff
Kevin Sheehan <ksheehan@liquidhub.com>
2018-02-20 12:58:00 -0500

    Initial commit

--- _NwnUtilLib/version.cpp
+++
@@ -1,4 +0,0 @@
-#include "_NwnUtilLib/version.h"
-
-const std::string gGIT_VERSION = "HEAD-HASH-NOTFOUND";
-const std::string gGIT_VERSION_SHORT = "HEAD-HASH-NOTFOUND.HEAD-HASH-NOTFOUND.HEAD-HASH-NOTFOUND";
