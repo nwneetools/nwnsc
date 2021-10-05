35925c765ba1d34acca24ba895be34278cd21b8a
meaglyn <meaglyn.nwn@gmail.com>
2021-10-05 15:19:48 -0400

    Support bison 3.6.0+
    Use macro games to work with newer bison, greater than
    3.6.0. Tested with 3.6.4 and 3.0.4.

--- _NscLib/NscCompat.h
+++ _NscLib/NscCompat.h
@@ -5,13 +5,22 @@
    the NWN_BISON_3 macro is defined by the CMakeLists.txt
    based on the version of bison found
 */
-#     define YYEMPTY		(-2)
-#ifdef NWN_BISON_3
+#ifdef NWN_BISON_3_6
+#   define YYCHAR_NAME yyla.kind()
+#   define YYLA_EMPTY yyla.clear()
 #   define YYLVAL yyla.value
-#   define YYCHAR_NAME yyla.type
+#   define YYLA_CHARNAME_LEX yyla.kind_ = yytranslate_(yylex(&YYLVAL,context))
 #else
-#   define  YYCHAR_NAME yychar
-#   define YYLVAL yylval
+#  define YYEMPTY              (-2)
+#  ifdef NWN_BISON_3
+#      define YYLVAL yyla.value
+#      define YYCHAR_NAME yyla.type
+#  else
+#      define YYCHAR_NAME yychar
+#      define YYLVAL yylval
+#  endif
+#  define YYLA_EMPTY  YYCHAR_NAME = YYEMPTY
+#  define YYLA_CHARNAME_LEX YYCHAR_NAME = yylex(&YYLVAL, context)
 #endif
 
 
